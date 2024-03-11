777f10fc3a217635a4ceb6679ef2fe016f35491d
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-03-11 18:27:25 -0500

    Added a route handler for /hbnb

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Flask Module for HBNB project.
+
+This module creates a simple Flask web application with a single route.
+The application listens on 0.0.0.0, port 5000, and returns "Hello HBNB!"
+when the root URL (/) is accessed. The use of `strict_slashes=False`
+in the route definition allows the URL to be accessed with or without
+a trailing slash.
+
+Additionally, will also handle /hbnb routes.
+
+"""
+
+from flask import Flask  # Corrected the import statement to Flask with a capital 'F'
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def index():
+    """
+    Index route handler.
+
+    Returns:
+        str: The string "Hello HBNB!" when the root URL is accessed.
+    """
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    HBNB route handler.
+
+    Returns
+        str: HBNB. Can you imagine?
+    """
+    return "HBNB"
+
+if __name__ == '__main__':
+    # Runs the Flask application on 0.0.0.0, listening on port 5000.
+    app.run(host='0.0.0.0', port=5000)
